b3eb47d1a62c53d5cd21bb9253d3c281b73aade2
CarlosMiguelAraujo <151648583+CarlosMiguelAraujo@users.noreply.github.com>
2023-12-06 21:03:35 -0300

    Update Vitrine.java

--- Atividade_Fabricio/src/com/atividade/Loja/Vitrine.java
+++ Atividade_Fabricio/src/com/atividade/Loja/Vitrine.java
@@ -1,5 +1,12 @@
-package com.atividade.Loja;
+package com.atividade.loja;
+import java.util.ArrayList;
 
 public class Vitrine {
-
+   ArrayList <Itens> itens = new ArrayList <>();
+     
+   
+	
+	 
+	 
+	 
 }
